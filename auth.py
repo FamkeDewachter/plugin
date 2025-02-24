9a001bdc8d9e9b2c08dc3273e75d7cc6a731af03
FamkeDewachter <famke.dewachter@hotmail.be>
2025-02-24 20:51:41 +0100

    ignore pycache files

--- auth.py
+++ auth.py
@@ -1,7 +1,6 @@
 import os
 import pickle
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 # If modifying these SCOPES, delete the file token.pickle.
