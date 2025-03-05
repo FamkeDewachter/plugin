34762725610f5b626fb844b8568fe3128c690b44
FamkeDewachter <famke.dewachter@hotmail.be>
2025-03-05 15:57:55 +0100

    UPDATE authenticator process for when cridentials have expired

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -1,7 +1,9 @@
 import os
 import pickle
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.exceptions import RefreshError
 
 # If modifying these SCOPES, delete the file token.pickle.
 SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
@@ -16,16 +18,35 @@ def authenticate_google_drive():
     if os.path.exists("token.pickle"):
         with open("token.pickle", "rb") as token:
             creds = pickle.load(token)
+
     # If there are no valid credentials, prompt the user to log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            try:
+                creds.refresh(Request())
+            except RefreshError:
+                # If the refresh token is invalid, delete the token.pickle file and re-authenticate.
+                os.remove("token.pickle")
+                # Path to credentials.json in the auth folder
+                credentials_path = os.path.join(
+                    os.path.dirname(__file__), "credentials.json"
+                )
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    credentials_path, SCOPES
+                )
+                creds = flow.run_local_server(port=0)
         else:
+            # Path to credentials.json in the auth folder
+            credentials_path = os.path.join(
+                os.path.dirname(__file__), "credentials.json"
+            )
             flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
+                credentials_path, SCOPES
             )
             creds = flow.run_local_server(port=0)
+
         # Save the credentials for the next run
         with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
+
     return creds
