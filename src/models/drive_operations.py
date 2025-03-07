02ecd91b5e15b703d1668b239afb02701b63167e
FamkeDewachter <famke.dewachter@hotmail.be>
2025-03-07 15:30:38 +0100

    add search files function

--- src/models/drive_operations.py
+++ src/models/drive_operations.py
@@ -18,11 +18,40 @@ class DriveOperations:
         Returns:
             A list of file dictionaries with 'id' and 'name' keys.
         """
+        query = "mimeType != 'application/vnd.google-apps.folder' and trashed = false"
+
         results = (
             self.service.files()
-            .list(pageSize=10, fields="nextPageToken, files(id, name)")
+            .list(
+                q=query, pageSize=10, fields="nextPageToken, files(id, name)"
+            )
             .execute()
         )
+
+        return results.get("files", [])
+
+    def search_files(self, search_term):
+        """
+        Searches for files in Google Drive by name.
+
+        Args:
+            search_term (str): The name or part of the file name to search.
+
+        Returns:
+            A list of file dictionaries with 'id' and 'name' keys.
+        """
+        query = f"name contains '{search_term}' and mimeType != 'application/vnd.google-apps.folder' and trashed = false"
+
+        results = (
+            self.service.files()
+            .list(
+                q=query,
+                pageSize=20,  # Show more results for search
+                fields="files(id, name)",
+            )
+            .execute()
+        )
+
         return results.get("files", [])
 
     def map_file_ids(self, files):
@@ -80,3 +109,27 @@ class DriveOperations:
         except Exception as error:
             print(f"An error occurred: {error}")
             return False
+
+    def get_file_content(self, file_id):
+        """
+        Fetches the content of a file given its ID.
+
+        Args:
+            file_id: The ID of the file to fetch content for.
+
+        Returns:
+            A tuple containing the content of the file and its MIME type.
+        """
+        try:
+            request = self.service.files().get_media(fileId=file_id)
+            file_content = request.execute()
+            file_metadata = (
+                self.service.files()
+                .get(fileId=file_id, fields="mimeType")
+                .execute()
+            )
+            mime_type = file_metadata.get("mimeType", "text/plain")
+            return file_content, mime_type
+        except Exception as error:
+            print(f"An error occurred: {error}")
+            return b"Failed to fetch file content.", "text/plain"
